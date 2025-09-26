b31cd4cebd5765abd19b34770cc2cd8cfc7f7086
nix_pardus <nix_pardus@bk.ru>
2025-09-27 01:45:32 +0300

    Добавьте файлы проекта.

---
+++ ParallelHomework/ArrayHelper.cs
@@ -0,0 +1,56 @@
+﻿namespace ParallelHomework;
+
+public static class ArrayHelper
+{
+    public static long Sum(int[] array)
+    {
+        long sum = 0;
+        foreach (var item in array)
+        {
+            sum += item;
+        }
+        return sum;
+    }
+
+    public static long SumWithListThreads(int[] array, int numberOfThreads)
+    {
+        if (numberOfThreads <= 0)
+        {
+            throw new ArgumentException("Number of threads must be greater than zero.");
+        }
+        int length = array.Length;
+        int chunkSize = (int)Math.Ceiling((double)length / numberOfThreads);
+        long[] sums = new long[numberOfThreads];
+        List<Thread> threads = new List<Thread>();
+        for (int i = 0; i < numberOfThreads; i++)
+        {
+            int threadIndex = i;
+            var thread = new Thread(() =>
+            {
+                int start = threadIndex * chunkSize;
+                int end = Math.Min(start + chunkSize, length);
+                for (int j = start; j < end; j++)
+                {
+                    sums[threadIndex] += array[j];
+                }
+            });
+            threads.Add(thread);
+            thread.Start();
+        }
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+        long totalSum = 0;
+        foreach (var partialSum in sums)
+        {
+            totalSum += partialSum;
+        }
+        return totalSum;
+    }
+
+    public static long SumWithPLINQ(int[] array)
+    {
+        return array.AsParallel().Sum(x => (long)x);
+    }   
+}
---
+++ ParallelHomework/Program.cs
@@ -0,0 +1,41 @@
+﻿using ParallelHomework;
+using System.Diagnostics;
+
+Console.WriteLine("Тест на 100 000");
+PrintResults(CreateArray(100_000));
+Console.WriteLine("Тест на 1 000 000");
+PrintResults(CreateArray(1_000_000));
+Console.WriteLine("Тест на 10 000 000");
+PrintResults(CreateArray(10_000_000));
+
+
+int[] CreateArray(int length)
+{
+    int[] arr = new int[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = i;
+    }
+
+    return arr;
+}
+
+void PrintResults(int[] arr)
+{
+    Stopwatch stopwatch = new Stopwatch();
+    stopwatch.Start();
+    long sum = ArrayHelper.Sum(arr);
+    stopwatch.Stop();
+    Console.WriteLine($"Sum: {sum}, Time taken (Single Thread): {stopwatch.ElapsedMilliseconds} ms");
+
+    stopwatch.Restart();
+    long sumWithListThreads = ArrayHelper.SumWithListThreads(arr, 4);
+    stopwatch.Stop();
+    Console.WriteLine($"Sum: {sumWithListThreads}, Time taken (4 List Threads): {stopwatch.ElapsedMilliseconds} ms");
+
+    stopwatch.Restart();
+    long sumWithPLINQ = ArrayHelper.SumWithPLINQ(arr);
+    stopwatch.Stop();
+    Console.WriteLine($"Sum: {sumWithPLINQ}, Time taken (PLINQ): {stopwatch.ElapsedMilliseconds} ms");
+}
